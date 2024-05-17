024139de6ece25b44b1aad37f7f3e6534d00f34c
Angel Beltran <100494810+abeltran09@users.noreply.github.com>
2024-05-17 00:12:22 -0500

    Created configuration file, added env variables, connected database, created user model and routes (#1)

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,11 +1,15 @@
 from flask import current_app as app
 from flask import Blueprint
+from flask import request
+from ..models.extensions import db
+from ..models.models import User
+import uuid
 
 temp_blueprint = Blueprint('temp_blueprint', __name__)
 
 
 
-@temp_blueprint.route('/')
-@temp_blueprint.route('/index')
+@temp_blueprint.route('/', methods=['POST'])
+@temp_blueprint.route('/index', methods=['POST'])
 def index():
     return "Hello, World!"
