96977d42866bac293c7008eef9fb831a2e547de9
Angel Beltran <aabeltran5910@gmail.com>
2024-05-20 22:12:51 -0500

    added jwt into user actions and login. Also cleaned up code

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,3 +8,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    JWT_TOKEN_LOCATION = ['headers', 'query_string']
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
--- app/routes/userRoutes.py
+++ app/routes/userRoutes.py
@@ -7,6 +7,7 @@ from ..models.models import User
 import uuid
 import json
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 bcrypt = Bcrypt()
 
@@ -15,9 +16,9 @@ user_blueprint = Blueprint('user_blueprint', __name__)
 @user_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
     form = request.get_json()
-    username = form["username"]
-    email = form["email"]
-    password = form["password"]
+    username = form.get("username")
+    email = form.get("email")
+    password = form.get("password")
     new_uuid = str(uuid.uuid4())
     hashed_password = bcrypt.generate_password_hash(password)
 
@@ -41,20 +42,22 @@ def register():
 def login():
     form = request.get_json()
     identifier = form.get("username") or form.get("email")
-    password = form["password"]
+    password = form.get("password")
 
     user = User.query.filter((User.username == identifier) | (User.email == identifier)).first()
 
     if user and bcrypt.check_password_hash(user.password_hash, password):
-        return jsonify({"message": "Login successful", "user_id": user.user_id}), 200, {'ContentType':'application/json'}
+        access_token = create_access_token(identity=identifier)
+        return jsonify({"message": "Login successful", "user_id": user.user_id, "access_token":access_token}), 200, {'ContentType':'application/json'}
     else:
         return jsonify({"message": "Unsuccessful Login"}), 401, {'ContentType':'application/json'}
 
 @user_blueprint.route('/delete', methods=['GET','DELETE', 'POST'])
+@jwt_required()
 def deleteUser():
     form = request.get_json()
     identifier = form.get("username") or form.get("email")
-    password = form["password"]
+    password = form.get("password")
 
     user = User.query.filter((User.username == identifier) | (User.email == identifier)).first()
 
@@ -70,11 +73,12 @@ def deleteUser():
         return jsonify({"message":"User deletion unsuccessful, check that username, email, and password are correct"})
 
 @user_blueprint.route('/update-username', methods=['GET', 'PATCH', 'POST'])
+@jwt_required()
 def updateUserUsername():
     form = request.get_json()
-    username = form["username"]
-    new_username = form["new_username"]
-    password = form["password"]
+    username = form.get("username")
+    new_username = form.get("new_username")
+    password = form.get("password")
 
     user = User.query.filter((User.username == username)).first()
 
@@ -90,11 +94,12 @@ def updateUserUsername():
         return jsonify({"message":"Make sure to enter username and password correctly"})
 
 @user_blueprint.route('/update-email', methods=['GET', 'PATCH', 'POST'])
+@jwt_required()
 def updateUserEmail():
     form = request.get_json()
-    email = form["email"]
-    new_email = form["new_email"]
-    password = form["password"]
+    email = form.get("email")
+    new_email = form.get("new_email")
+    password = form.get("password")
 
     user = User.query.filter((User.email == email)).first()
 
@@ -111,11 +116,12 @@ def updateUserEmail():
 
 
 @user_blueprint.route('/update-password', methods=['GET', 'PATCH', 'POST'])
+@jwt_required()
 def updateUserPassword():
     form = request.get_json()
     identifier = form.get("username") or form.get("email")
-    old_password = form["old_password"]
-    new_password = form["new_password"]
+    old_password = form.get("old_password")
+    new_password = form.get("new_password")
 
     user = User.query.filter((User.username == identifier) | (User.email == identifier)).first()
 
@@ -130,8 +136,3 @@ def updateUserPassword():
             return jsonify({"message":str(e)}), 400
     else:
         return jsonify({"message":"Make sure to enter username or email and old password correctly"})
-
-
-    
-
-
